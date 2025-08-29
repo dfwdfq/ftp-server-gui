7a0766c90db16c8cbb69192f68330726f2737b29
hdvpdrm <hardcoderyan@gmail.com>
2025-08-29 08:54:23 +0000

    entry point to app

--- __main__.py
+++ __main__.py
@@ -1,3 +1,8 @@
 
+from core.args import get_arguments
 if __name__ == '__main__':
-    pass
+    arguments = get_arguments()
+    if "TUI" == arguments.mode:
+        print("starting FTP server in TUI mode.")
+    if "GUI" == arguments.mode:
+        print("starting FTP server in GUI mode.")

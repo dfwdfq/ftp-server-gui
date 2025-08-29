5aab2c5ed3da8cfcda36561f0a6ea5da397f65f6
hdvpdrm <hardcoderyan@gmail.com>
2025-08-29 07:47:29 +0000

    this is entry point

--- __main__.py
+++ __main__.py
@@ -1,14 +1,3 @@
-import sys
-from PyQt5.QtWidgets import QApplication
-from config_manager import load_config
-from gui import FTPGuiApp
 
 if __name__ == '__main__':
-    config = load_config()
-    app = QApplication(sys.argv)
-    gui = FTPGuiApp(config)
-    
-    if not config.getboolean('FTP', 'run_as_daemon'):
-        gui.show()
-
-    sys.exit(app.exec_())
+    pass
